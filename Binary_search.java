fb61934eb39b50bf2829057bae3cf9a8be8641fc
Piyush <piyushpratapsingh67@gmail.com>
2024-03-26 18:02:33 +0530

    Change in Binary Search Code.

--- Binary_search.java
+++ Binary_search.java
@@ -1,28 +1,33 @@
 public class Binary_search {
     public static void main(String[] args) {
         int[] arr = { 4, 5, 6, 8, 10, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22 };
-        int element = 4;
+        int element = 12;
         System.out.println(search_binary(arr, element));
     }
 
-    public static int search_binary(int[] array, int x) {
-        int low = 0;
-        int high = array.length - 1;
+    public static int search_binary(int[] arr, int element) {
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-      
-            if (array[mid] == x)
-              return mid;
-      
-            if (array[mid] < x)
-              low = mid + 1;
-      
-            else
-              high = mid - 1;
-          }
+      int start = 0;
+      int end = arr.length - 1;
 
-        return -1;
+      while(start <= end) {
+        int mid = (start + end) / 2;
+        if(arr[mid] == element) { //found the element.
+          return mid;
+        } 
+
+        if (arr[mid] < element) {
+          //right.
+
+          start = mid + 1;
+        } else {
+          //left.
+
+          end = mid - 1;
+        }
+      }
+
+      return -1;
     }
 
 }
