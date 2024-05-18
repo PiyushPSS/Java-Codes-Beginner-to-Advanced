7c38aab1f3de6c8a537f5dbb3ef156468c52df85
Piyush <piyushpratapsingh67@gmail.com>
2024-05-18 11:59:16 +0530

    Removing unnecessary programs

--- may13.java
+++
@@ -1,8 +0,0 @@
-import testing.test;
-
-public class may13 {
-    public static void main(String[] args) {
-        test newTest = new test();
-        newTest.main(args);
-    }
-}
--- may15.java
+++
@@ -1,9 +0,0 @@
-import testing.*;
-
-public class may15 {
-    public static void main(String[] args) {
-        test newTest = new test();
-        newTest.main(args);
-    }
-}
-`
